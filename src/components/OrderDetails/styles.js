9b5c79a440fd9a186cb67440ac1a3ec1da3eee4a
Jesus David <50583541+YisusJe@users.noreply.github.com>
2022-01-31 10:28:24 -0400

    [ADD] advanced offers and discounts (#878)
    * [ADD] advanced offers and discounts
    
    * [add] remove offer

--- src/components/OrderDetails/styles.js
+++ src/components/OrderDetails/styles.js
@@ -709,3 +709,15 @@ export const Exclamation = styled.div`
   margin-left: 5px;
   top: 3px;
 `
+
+export const Divider = styled.div`
+  border-top: 1px solid #EAEAEA;
+  margin-bottom: 10px;
+`
+
+export const CommentContainer = styled.div`
+  margin: 0;
+  h3 {
+    margin: 0;
+  }
+`
